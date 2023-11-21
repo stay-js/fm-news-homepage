9a7a93961312bca139839090c82d374fff49831a
Nagy Zétény <nagyzeteny6@gmail.com>
2023-11-21 11:52:41 +0100

    desktop layout

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { cn } from '~/utils/cn';
-import { navItems } from '~/constants/nav-items';
 
 export const Navigation: React.FC = () => {
   const [isToggled, setIsToggled] = useState<boolean>(false);
@@ -21,34 +20,29 @@ export const Navigation: React.FC = () => {
   };
 
   return (
-    <nav className="flex h-20 w-full select-none items-center">
+    <nav className="flex w-full select-none items-center pb-12 pt-8 lg:pt-24">
       <div className="content flex w-full items-center justify-between">
         <img src="/logo.svg" alt="Logo" />
 
-        <button
-          className="h-6 w-6 lg:hidden"
-          title="Toggle Hamburger"
-          type="button"
-          onClick={handleToggle}
-        >
+        <button className="lg:hidden" title="Toggle Hamburger" type="button" onClick={handleToggle}>
           <img src={isToggled ? '/icon-menu-close.svg' : '/icon-menu.svg'} alt="Toggle button" />
         </button>
 
         <div
           className={cn(
-            'fixed left-0 top-20 h-[calc(100vh-5rem)] w-full overflow-y-auto lg:static lg:flex lg:h-fit lg:w-fit',
+            'fixed right-0 top-0 h-screen w-[80%] lg:static lg:flex lg:h-fit lg:w-fit',
             !isToggled && 'hidden',
           )}
         >
-          <ul className="content flex flex-col gap-8 pb-20 pt-4 lg:w-fit lg:flex-row lg:gap-1 lg:p-0">
-            {navItems.map(({ path, name }) => (
-              <li key={path}>
+          <ul className="flex flex-col gap-8 pb-20 pt-4 lg:w-fit lg:flex-row lg:gap-8 lg:p-0">
+            {['Home', 'New', 'Popular', 'Trending', 'Categories'].map((item) => (
+              <li key={item}>
                 <a
-                  className="relative flex items-center gap-2 font-bold text-black transition-colors after:absolute after:-bottom-4 after:h-px after:w-full after:bg-neutral-300 lg:static lg:rounded-md lg:px-3 lg:py-2 lg:font-medium lg:after:hidden lg:hover:bg-neutral-300"
+                  className="text-gray-500 transition-colors hover:text-soft-red"
                   onClick={handleClose}
-                  href={path}
+                  href="#"
                 >
-                  {name}
+                  {item}
                 </a>
               </li>
             ))}
